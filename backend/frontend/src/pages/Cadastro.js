1450d2f9823a8d85744225dfcc42eb36515b0a94
Guilherme <guissantana105@gmail.com>
2022-06-27 00:47:30 -0400

    conectando front/back

--- backend/frontend/src/pages/Cadastro.js
+++ backend/frontend/src/pages/Cadastro.js
@@ -1,22 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "../style/cadastro.css";
+import { get } from "mongoose";
 
 export default function Cadastro() {
+  const [list, setlist] = useState([]);
+  async function getData() {
+    const result = await axios.get("http://localhost:5000/api/pessoas");
+    setlist(result.data);
+    console.log(result.data);
+  }
+  useEffect(() => {
+    getData();
+  }, []);
+  async function deleteData(id) {
+    await axios.delete("http://localhost:5000/api/pessoas.delete/" + id);
+    getData();
+  }
+  async function updateData(id, nome, apelido, email, telefone, endereco) {
+    await axios.put("http://localhost:5000/api/pessoas.update/" + id, {
+      nome, apelido, email, telefone, endereco});
+      getData();
+  }
   return (
     <div className="container-cadastro">
       <div className="container-cadatro-form">
         <h1>Cadastrar</h1>
-        <input type="text" placeholder="Nome" /><br/>
-        <input type="text" placeholder="Sobrenome" /><br/>
-        <input type="text" placeholder="Telefone" /><br/>
-        <input type="text" placeholder="Email" /><br/>
-        <input type="text" placeholder="Endereço" /><br/>
+        <input type="text" placeholder="Nome" />
+        <br />
+        <input type="text" placeholder="Sobrenome" />
+        <br />
+        <input type="text" placeholder="Telefone" />
+        <br />
+        <input type="text" placeholder="Email" />
+        <br />
+        <input type="text" placeholder="endereco" />
+        <br />
         <button className="login-cadatro-btn">Cadastrar</button>
       </div>
 
       <div className="container-todolist">
-        <h1>Lista Pessoas</h1>
-        
+        <br />
+        {list.length > 0 &&
+          list.map((item) => (
+            <>
+              <div>
+                <li>
+                  name: {item.nome}
+                  <br />
+                  Apelido: {item.apelido}
+                  <br />
+                  Telefone: {item.telefone}
+                  <br />
+                  Email: {item.email} <br />
+                  Endereço: {item.endereco}<br />
+                </li>
+                <button className="login-cadatro-btn" onClick={()=> deleteData(item._id)}>🗑️</button>
+                <button className="login-cadatro-btn" onClick={()=> updateData(item._id)}>✏️</button>
+              </div>
+              <br />
+            </>
+          ))}
       </div>
     </div>
   );
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,21 +5,19 @@ const routes = express.Router();
 const Usuario = require('../src/controllers/usuario.controller');
 const Pessoa = require('../src/controllers/pessoa.controller');
 
-routes.get('/', Usuario.index);
-
 
 //Rotas de Usuarios
 routes.post('/api/usuarios', Usuario.create); //Cria um novo usuário
-routes.get('/usuarios.create', Usuario.index); //Lista todos os usuários
-routes.get('/usuarios.details/:_id',Usuario.details); //Lista um usuário específico
-routes.delete('/usuarios.delete/:_id', Usuario.delete); //Deleta um usuário
-routes.put('/usuarios.update', Usuario.update); //Atualiza um usuário
+routes.get('/api/usuarios.create', Usuario.index); //Lista todos os usuários
+routes.get('/api/usuarios.details/:_id',Usuario.details); //Lista um usuário específico
+routes.delete('/api/usuarios.delete/:_id', Usuario.delete); //Deleta um usuário
+routes.put('/api/usuarios.update', Usuario.update); //Atualiza um usuário
 
 //Rotas de Pessoas
-routes.post('/pessoas.create', Pessoa.create); //Criar uma nova pessoa
+routes.post('/api/pessoas.create', Pessoa.create); //Criar uma nova pessoa
 routes.get('/api/pessoas', Pessoa.index); //Listar todas as pessoas
-routes.get('/pessoas.details/:_id', Pessoa.details); //Detalhar uma pessoa
-routes.delete('/pessoas.delete/:_id', Pessoa.delete); //Deletar uma pessoa
-routes.put('/pessoas.update', Pessoa.update); //Atualizar uma pessoa
+routes.get('/api/pessoas.details/:_id', Pessoa.details); //Detalhar uma pessoa
+routes.delete('/api/pessoas.delete/:_id', Pessoa.delete); //Deletar uma pessoa
+routes.put('/api/pessoas.update', Pessoa.update); //Atualizar uma pessoa
 
 module.exports = routes;
